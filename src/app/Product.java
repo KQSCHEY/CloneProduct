9da0e18d9548c17080dc547c4a3668adcfcb1206
Alexander Kascheiev <102923209+KQSCHEY@users.noreply.github.com>
2022-05-29 19:18:24 +0300

    Add prise

--- src/app/Product.java
+++ src/app/Product.java
@@ -5,10 +5,12 @@ public class Product {
 
     private final String name;
     private final double weight;
+    private final double prise;
 
     public Product(String name, double weight) {
         this.name = name;
         this.weight = weight;
+        this.prise = prise;
     }
 
     public String getName() {
@@ -17,5 +19,9 @@ public class Product {
 
     public double getWeight() {
         return weight;
+    }
+
+    public double getPrise() {
+        return prise;
     }
 }
